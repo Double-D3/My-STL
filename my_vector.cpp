5ab2d92a4845805fc048e9f75d564d75f52eaca2
Double-D3 <141055536+Double-D3@users.noreply.github.com>
2023-07-31 19:42:02 +0300

    Create my_vector.cpp

---
+++ my_vector.cpp
@@ -0,0 +1,511 @@
+#include <iostream> 
+
+namespace MySTL
+{
+	template<class T>
+	class my_vector
+	{
+	private:
+		T* array;
+		unsigned int Size;//количество элементов в векторе
+		unsigned int Capacity;//количество выделенных ячеек в векторе
+		static double CAPACITY_INCREASE_FACTOR; // опеределяет количество дополнительных ячеек в векторе (30%)
+		bool increase_capacity();
+		void check(int subscript);
+	public:
+		class iterator;
+		my_vector();
+		my_vector(const unsigned int size);
+		my_vector(const unsigned int size, T element);
+		my_vector(const my_vector& other);
+		my_vector(my_vector&& other);
+		~my_vector();
+		T& operator[](const int& i);
+		int size();
+		int capacity();
+		void push_back(const T& New_Element);
+		void pop_back();
+		void reserve(unsigned int capacity);
+		T& at(const unsigned int position);
+		void clear();
+		bool empty();
+		iterator begin();
+		iterator end();
+		void insert(iterator Iterator, const T& element);
+		void erase(iterator Iterator);
+		void swap(my_vector<T>& right);
+		my_vector<T>& operator=(const my_vector<T>& right);
+	};
+
+	template<class T>
+	class my_vector<T>::iterator
+	{
+	private:
+		T* ptr;
+		T* vector_begin;
+		T* vector_end;
+		void access_check();
+		void check_iterator();
+		void check_erase_iterator();
+	public:
+		friend my_vector;
+		iterator();
+		iterator(const typename my_vector<T>::iterator& other);
+		iterator(typename my_vector<T>::iterator&& other)noexcept;
+		~iterator();
+		T& operator*();
+		typename my_vector<T>::iterator& operator++();
+		typename my_vector<T>::iterator operator++(int);
+		typename my_vector<T>::iterator& operator--();
+		typename my_vector<T>::iterator operator--(int);
+		typename my_vector<T>::iterator& operator=(const typename my_vector<T>::iterator& other);
+		typename my_vector<T>::iterator operator+(const int shift);
+		typename my_vector<T>::iterator operator-(const int shift);
+		typename my_vector<T>::iterator operator+=(const int shift);
+		typename my_vector<T>::iterator operator-=(const int shift);
+		bool operator!=(const typename my_vector<T>::iterator& other);
+		bool operator<(const typename my_vector<T>::iterator& other);
+		bool operator>(const typename my_vector<T>::iterator& other);
+		bool operator<=(const typename my_vector<T>::iterator& other);
+		bool operator>=(const typename my_vector<T>::iterator& other);
+		bool operator==(const typename my_vector<T>::iterator& other);
+	};
+
+	//////////////////////////iterator/////////////////////////
+	template<class T>
+	void my_vector<T>::iterator::access_check()
+	{
+		if (this->ptr < this->vector_begin || this->ptr >= (this->vector_end))
+		{
+			std::cout << std::endl << "can't dereference out of range my_vector iterator";
+			abort();
+		}
+	}
+
+	template<class T>
+	void my_vector<T>::iterator::check_iterator()
+	{
+		if (this->ptr < this->vector_begin || this->ptr >(this->vector_end))
+		{
+			std::cout << std::endl << "you can't increment (or decrement) an iterator beyond a range";
+			abort();
+		}
+	}
+
+	template<class T>
+	void my_vector<T>::iterator::check_erase_iterator()
+	{
+		if (this->ptr == this->vector_end)
+		{
+			std::cout << std::endl << "my_vector erase iterator outside range";
+			abort();
+		}
+	}
+
+	template<class T>
+	bool my_vector<T>::iterator::operator!=(const typename my_vector<T>::iterator& other)
+	{
+		return this->ptr != other.ptr;
+	}
+
+	template<class T>
+	bool my_vector<T>::iterator::operator<(const typename my_vector<T>::iterator& other)
+	{
+		return this->ptr < other.ptr;
+	}
+
+	template<class T>
+	bool my_vector<T>::iterator::operator>(const typename my_vector<T>::iterator& other)
+	{
+		return this->ptr > other.ptr;
+	}
+
+	template<class T>
+	bool my_vector<T>::iterator::operator<=(const typename my_vector<T>::iterator& other)
+	{
+		return this->ptr <= other.ptr;
+	}
+
+	template<class T>
+	bool my_vector<T>::iterator::operator>=(const typename my_vector<T>::iterator& other)
+	{
+		return this->ptr >= other.ptr;
+	}
+
+	template<class T>
+	bool my_vector<T>::iterator::operator==(const typename my_vector<T>::iterator& other)
+	{
+		return this->ptr == other.ptr;
+	}
+
+	template<class T>
+	typename my_vector<T>::iterator my_vector<T>::iterator::operator+=(const int shift)
+	{
+		this->ptr += shift;
+		this->check_iterator();
+		return *this;
+	}
+
+	template<class T>
+	typename my_vector<T>::iterator my_vector<T>::iterator::operator-=(const int shift)
+	{
+		this->ptr -= shift;
+		this->check_iterator();
+		return *this;
+	}
+
+	template<class T>
+	typename my_vector<T>::iterator my_vector<T>::iterator::operator+(const int shift)
+	{
+		typename my_vector<T>::iterator New_iterator = *this;
+		New_iterator.ptr += shift;
+		New_iterator.check_iterator();
+		return New_iterator;
+	}
+
+	template<class T>
+	typename my_vector<T>::iterator my_vector<T>::iterator::operator-(const int shift)
+	{
+		typename my_vector<T>::iterator New_iterator = *this;
+		New_iterator.ptr -= shift;
+		New_iterator.check_iterator();
+		return New_iterator;
+	}
+
+	template<class T>
+	typename my_vector<T>::iterator& my_vector<T>::iterator::operator++()
+	{
+		this->ptr++;
+		this->check_iterator();
+		return *this;
+	}
+
+	template<class T>
+	typename my_vector<T>::iterator my_vector<T>::iterator::operator++(int)
+	{
+		typename my_vector<T>::iterator New_iterator = *this;
+		this->ptr++;
+		this->check_iterator();
+		return New_iterator;
+	}
+
+	template<class T>
+	typename my_vector<T>::iterator& my_vector<T>::iterator::operator--()
+	{
+		this->ptr--;
+		this->check_iterator();
+		return *this;
+	}
+
+	template<class T>
+	typename my_vector<T>::iterator my_vector<T>::iterator::operator--(int)
+	{
+		typename my_vector<T>::iterator New_iterator = *this;
+		this->ptr--;
+		this->check_iterator();
+		return New_iterator;
+	}
+
+	template<class T>
+	typename my_vector<T>::iterator& my_vector<T>::iterator::operator=(const typename my_vector<T>::iterator& other)
+	{
+		this->ptr = other.ptr;
+		this->vector_begin = other.vector_begin;
+		this->vector_end = other.vector_end;
+		return *this;
+	}
+
+	template<class T>
+	T& my_vector<T>::iterator::operator*()
+	{
+		this->access_check();
+		return *(this->ptr);
+	}
+
+	template<class T>
+	my_vector<T>::iterator::~iterator() {}
+
+	template<class T>
+	my_vector<T>::iterator::iterator()
+	{
+		this->ptr = this->vector_begin = this->vector_end = nullptr;
+	}
+
+	template<class T>
+	my_vector<T>::iterator::iterator(const typename my_vector<T>::iterator& other)
+	{
+		*this = other;
+	}
+
+	template<class T>
+	my_vector<T>::iterator::iterator(typename my_vector<T>::iterator&& other)noexcept
+	{
+		*this = other;
+	}
+	//////////////////////////iterator/////////////////////////
+
+	//////////////////////////my_vector/////////////////////////
+	template<class T>
+	void my_vector<T>::insert(typename my_vector<T>::iterator Iterator, const T& element)
+	{
+		this->Size++;
+		if (this->increase_capacity())
+		{
+			T* New_Array = new T[this->Capacity]();
+			typename my_vector<T>::iterator it = this->begin();
+			size_t index = 0;
+			for (; it < Iterator; ++it, ++index)
+			{
+				New_Array[index] = this->array[index];
+			}
+			New_Array[index] = element;
+			++index;
+			for (; index < this->Size; index++)
+			{
+				New_Array[index] = this->array[index - 1];
+			}
+			delete[]this->array;
+			this->array = New_Array;
+			New_Array = nullptr;
+		}
+		else
+		{
+			typename my_vector<T>::iterator end_iterator = this->end() - 1;
+			for (; end_iterator != Iterator; --end_iterator)
+			{
+				*(end_iterator) = *(end_iterator - 1);
+			}
+			*Iterator.ptr = element;
+		}
+	}
+
+	template<class T>
+	void my_vector<T>::erase(typename my_vector<T>::iterator Iterator)
+	{
+		Iterator.check_erase_iterator();
+		this->Size--;
+		typename my_vector<T>::iterator end_iterator = this->end();
+		size_t index = 0;
+		for (; Iterator < end_iterator; ++Iterator)
+		{
+			*Iterator = *(Iterator + 1);
+		}
+	}
+
+	template<class T>
+	void my_vector<T>::swap(my_vector<T>& right)
+	{
+		my_vector<T> new_vector = right;
+		right = *this;
+		*this = new_vector;
+	}
+
+	template<class T>
+	my_vector<T>& my_vector<T>::operator=(const my_vector<T>& right)
+	{
+		if (this->array != nullptr)
+			delete[]this->array;
+	
+		this->array = new T[right.Capacity]();
+		this->Capacity = right.Capacity;
+		this->Size = right.Size;
+		for (size_t index = 0; index < this->Size; index++)
+		{
+			this->array[index] = right.array[index];
+		}
+		return *this;
+	}
+
+	template<class T>
+	typename my_vector<T>::iterator my_vector<T>::end()
+	{
+		typename my_vector<T>::iterator New_iterator;
+		New_iterator.ptr = New_iterator.vector_end = this->array + this->Size;
+		New_iterator.vector_begin = this->array;
+		return New_iterator;
+	}
+
+	template<class T>
+	typename my_vector<T>::iterator my_vector<T>::begin()
+	{
+		typename my_vector<T>::iterator New_iterator;
+		New_iterator.ptr = this->array;
+		New_iterator.vector_begin = this->array;
+		New_iterator.vector_end = this->array + this->Size;
+		return New_iterator;
+	}
+
+	template<class T>
+	bool my_vector<T>::empty()
+	{
+		if (this->Size == 0)
+			return true;
+		else return false;
+	}
+
+	template<class T>
+	void my_vector<T>::pop_back()
+	{
+		if (this->Size != 0)
+		{
+			this->array[this->Size - 1] = T();
+			this->Size--;
+		}
+		else return;
+	}
+
+	template<class T>
+	void my_vector<T>::clear()
+	{
+		if (this->array != nullptr)
+		{
+			delete[]this->array;
+			this->array = nullptr;
+			this->Size = this->Capacity = 0;
+		}
+	}
+
+	template<class T>
+	void my_vector<T>::reserve(unsigned int capacity)
+	{
+		this->Capacity = capacity;
+		this->array = new T[capacity];
+	}
+
+	template<class T>
+	T& my_vector<T>::at(const unsigned int position)
+	{
+		this->check(position);
+		return this->array[position];
+	}
+
+	template<class T>
+	bool my_vector<T>::increase_capacity()//true - необходимо перезаписывать вектор в новую область памяти, false - нет необходимости перезаписывать данные вектора
+	{
+		const int EXTRA_CELL = 1;
+		if (this->Size > this->Capacity)// если новое количество элементов в векторе больше зарезервированного
+		{
+			this->Capacity = this->Size + (int)(this->Size * this->CAPACITY_INCREASE_FACTOR + EXTRA_CELL);
+			return true;
+		}
+		else return false;
+	}
+
+	template<class T>
+	void my_vector<T>::check(int subscript)
+	{
+		if (subscript < 0 || subscript >= Size)
+		{
+			std::cout << std::endl << "my_vector subscript out of range";
+			abort();
+		}
+	}
+
+	template<class T>
+	void my_vector<T>::push_back(const T& New_Element)
+	{
+		this->Size++;
+		if (this->increase_capacity())//если пора увеличивать this->Capacity, то перезаписываем данные в новый массив
+		{
+			T* New_Array = new T[this->Capacity]();
+			for (size_t index = 0; index < Size - 1; index++)
+			{
+				New_Array[index] = this->array[index];
+			}
+			New_Array[Size - 1] = New_Element;
+			delete[]this->array;
+			this->array = New_Array;
+			New_Array = nullptr;
+		}
+		else this->array[Size - 1] = New_Element;
+	}
+
+	template<class T>
+	int my_vector<T>::capacity()
+	{
+		return this->Capacity;
+	}
+
+	template<class T>
+	int my_vector<T>::size()
+	{
+		return this->Size;
+	}
+
+	template<class T>
+	my_vector<T>::my_vector()
+	{
+		const int EMPTY = 0;
+		this->array = nullptr;
+		this->Size = this->Capacity = EMPTY;
+	}
+
+	template<class T>
+	my_vector<T>::my_vector(const unsigned int size) :my_vector<T>::my_vector()
+	{
+		if (size > 0)
+		{
+			this->Size = this->Capacity = size;
+			this->array = new T[size];
+			for (unsigned int i = 0; i < size; i++)
+			{
+				this->array[i] = T();
+			}
+		}
+		else this->check(size);
+	}
+
+	template<class T>
+	my_vector<T>::my_vector(const unsigned int size, T element) :my_vector<T>::my_vector()
+	{
+		if (size > 0)
+		{
+			this->Size = this->Capacity = size;
+			this->array = new T[size];
+			for (unsigned int i = 0; i < size; i++)
+			{
+				this->array[i] = element;
+			}
+		}
+		else this->check(size);
+	}
+
+	template<class T>
+	my_vector<T>::my_vector(const my_vector& other)
+	{
+		*this = other;
+	}
+
+	template<class T>
+	my_vector<T>::my_vector(my_vector&& other)
+	{
+		this->Capacity = other.Capacity;
+		this->Size = other.Size;
+		this->array = other.array;
+		other.array = nullptr;
+	}
+
+	template<class T>
+	my_vector<T>::~my_vector()
+	{
+		if (this->array != nullptr)
+		{
+			delete[]this->array;
+			this->Size = this->Capacity = 0;
+		}
+	}
+
+	template<class T>
+	T& my_vector<T>::operator[](const int& i)
+	{
+		return array[i];
+	}
+	template<class T>
+	double my_vector<T>::CAPACITY_INCREASE_FACTOR = 0.3;
+	//////////////////////////my_vector/////////////////////////
+};
+
+int main()
+{
+  return 0;
+}
